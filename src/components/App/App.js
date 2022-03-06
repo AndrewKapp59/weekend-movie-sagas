320d19b3df736bb59f56690e836d46e573880e95
Andrew Kapp <dkapp59@gmail.com>
2022-03-05 19:05:09 -0600

    Adding a MovieForm component to let the user add new movies

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,7 @@ import {HashRouter as Router, Route, Link} from 'react-router-dom';
 import './App.css';
 import MovieList from '../MovieList/MovieList'
 import MovieDetails from '../MovieDetails/MovieDetails';
+import MovieForm from '../MovieForm/MovieForm';
 
 import Button from '@mui/material/Button';
 
@@ -16,6 +17,9 @@ function App() {
         <Button variant="text">
           <Link to="/details" style={{ textDecoration: 'none', color: 'white' }}>Details</Link>
         </Button>
+        <Button variant="text">
+          <Link to="/form" style={{ textDecoration: 'none', color: 'white' }}>Add Movie</Link>
+        </Button>
       </div>
         <h1>The Movies Saga!</h1>
         {/* Home */}
@@ -26,8 +30,8 @@ function App() {
         <Route path="/details" exact>
           <MovieDetails/>
         </Route>
-        
         {/* Add Movie page */}
+        <MovieForm />
       </Router>
     </div>
   );
---
+++ src/components/MovieForm/MovieForm.jsx
@@ -0,0 +1,28 @@
+import { TextField, Button } from "@mui/material";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+
+
+const MovieForm = () => {
+  const 
+  
+  
+  return(
+    <>
+      <form onSubmit={handleSubmit} className='movie-form'>
+        <TextField
+          placeholder='Enter Title'
+          onChange={(event) => {
+            setTitle(event.target.value);
+          }}
+          size='small'
+        />
+        <Button type='submit' variant='contained'>
+            Submit
+        </Button>
+      </form>
+    </>
+  )
+}
+
+export default MovieForm;
